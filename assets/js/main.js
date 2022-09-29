40e1e2b8223aade1d4cbe3d9acac6b15cbacca55
Ian Ham <ian.ham.ram@gmail.com>
2022-09-29 17:09:07 -0500

    modificacion de estilos a elementos existentes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 const menuIcon = document.querySelector(".header__menuIcon");
 const menu = document.querySelector(".menu");
+const header = document.querySelector(".header");
 
 menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("active");
     menu.classList.toggle("active");
+    header.classList.toggle("active");
 });
