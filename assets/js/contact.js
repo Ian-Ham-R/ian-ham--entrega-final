f853c5db5d6674dd270ae05b4f6c9b7c2c29f072
Ian Ham <ian.ham.ram@gmail.com>
2022-10-03 17:31:08 -0500

    creacion completa de pagina contact y modificacion de estilos a elemeâentos existent

---
+++ assets/js/contact.js
@@ -0,0 +1,7 @@
+
+const container = document.querySelector(".container");
+const signUpBtn = document.querySelector(".background button");
+
+signUpBtn.addEventListener("click", () => {
+    container.classList.toggle("change");
+});
